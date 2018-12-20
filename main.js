0eda54be9f8c2237a92199bab817d97ea873ad6e
Konstantin Meshavkin <meshavkin1996@gmail.com>
2018-12-20 23:57:34 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const {app, BrowserWindow} = require('electron');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+    win.loadFile('index.html');
+
+    win.on('closed', () => { win = null });
+}
+
+app.on('ready', createWindow);
