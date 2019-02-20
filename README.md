48ac19de328dec1266159a36117658611009c070
Kostyan1996 <meshavkin1996@gmail.com>
2019-02-20 03:31:50 +0300

    All buttons now work as intended

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Calculator
+## Description
+Mainly I created this project just to start with GitHub and demonstrate some basic stuff that I can do. So far avoiding any libraries intentionally.
+
+## Project focus
+Here's list of why I created this particular project and some key features of it:
+* Some newer features of CSS like Grid and Custom Properties
+* How to use GitHub in practice (used Bitbucket and GitLab before)
+* How to use VSCode with emmet and some useful extensions like Live Server, GitLens and ESLint (airbnb style) in practice
--- index.js
+++ index.js
@@ -1,61 +1,84 @@
-let value = [];
+function setMemory() {
+  return {
+    sign: '',
+    value: '',
+    prevValue: '',
+    memValue: '',
+    equalsBefore: false,
+  };
+}
+
+let m = setMemory();
+
+function count(val1, val2, sign) {
+  let val;
+  if (sign === '+') val = parseFloat(val1) + parseFloat(val2);
+  else if (sign === '-') val = parseFloat(val1) - parseFloat(val2);
+  else if (sign === '*') val = parseFloat(val1) * parseFloat(val2);
+  else if (sign === '÷') val = parseFloat(val1) / parseFloat(val2);
+  return val;
+}
 
 function formatSetScreen() {
-  document.getElementById('digits').innerText = value.join(' ');
+  document.getElementById('digits').innerText = m.value;
 }
 
 function push(val) {
-  // if (screen.innerText.length < 12) screen.innerText += val;
-  const lastVal = value[value.length - 1];
-  if (lastVal && lastVal.search(/[0-9,]/) !== -1) {
-    value[value.length - 1] += val;
-  } else {
-    value.push(val);
+  // If any operation button was already pressed and there's no values in memory yet, empty screen
+  // and remember value
+  if (m.sign && !m.prevValue) {
+    m.prevValue = m.value;
+    m.value = '';
   }
-}
-
-function clear() {
-  value = [];
-}
-
-function backspace() {
-  const val = value[value.length - 1];
-  if (val.search(/[0-9,]/) !== -1) value[value.length - 1] = val.slice(0, -1);
-}
-
-function add() {
-  const lastVal = value[value.length - 1];
-  if (lastVal && lastVal.search(/[0-9,]/) !== -1) {
-    value.push('+');
-  }
-}
-
-function substract() {
-  const lastVal = value[value.length - 1];
-  if (lastVal && lastVal.search(/[0-9,]/) !== -1) {
-    value.push('-');
-  }
+  m.value += val;
 }
 
 function equals() {
-  let result = parseFloat(value[0]);
-  for (let i = 1; i < value.length - 1; i++) {
-    if (value[i] === '+') {
-      result += parseFloat(value[i + 1]) || 0;
-    } else if (value[i] === '-') {
-      result -= parseFloat(value[i + 1]) || 0;
-    }
+  // If you press equals more than once, it will remember value after first press
+  if (!m.equalsBefore) {
+    m.memValue = m.value;
+    m.value = count(m.prevValue, m.value, m.sign) || m.value;
+  } else {
+    m.value = count(m.value, m.memValue, m.sign) || m.value;
   }
-  value = [result];
+  m.equalsBefore = true;
+}
+
+function applySign(val) {
+  // If same operation is used again, count it as "equals"
+  if (m.sign !== val) {
+    m.equalsBefore = false;
+    m.prevValue = '';
+  } else if (m.sign) {
+    equals();
+  }
+  m.sign = val;
+}
+
+function root() {
+  m.value = Math.sqrt(m.value);
+}
+
+function plusMinus() {
+  m.value *= -1;
+}
+
+function clear() {
+  m = setMemory();
+}
+
+function backspace() {
+  m.value = m.value.slice(0, -1);
 }
 
 document.getElementById('container').onclick = (event) => {
   const tgt = event.target.classList;
-  if (tgt.contains('number')) push(event.target.innerText);
+  if (tgt.contains('number') || tgt.contains('dot')) push(event.target.innerText);
+  else if (tgt.contains('equals')) equals();
+  else if (tgt.contains('sign')) applySign(event.target.innerText);
+  else if (tgt.contains('root')) root();
+  else if (tgt.contains('plusMinus')) plusMinus();
   else if (tgt.contains('clear')) clear();
   else if (tgt.contains('backspace')) backspace();
-  else if (tgt.contains('add')) add();
-  else if (tgt.contains('substract')) substract();
-  else if (tgt.contains('equals')) equals();
   formatSetScreen();
 };
