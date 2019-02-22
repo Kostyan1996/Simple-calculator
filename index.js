25771c77c2da7754179d7d96b41668400c302398
Kostyan1996 <meshavkin1996@gmail.com>
2019-02-22 03:16:46 +0300

    Cleared repo

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-(function calculator() {
-  let value = '';
+let value = [];
 
-  function formatSetScreen() {
-    document.getElementById('digits').innerText = value;
+function formatSetScreen() {
+  document.getElementById('digits').innerText = value.join('');
+}
+
+function push(val) {
+  // if (screen.innerText.length < 12) screen.innerText += val;
+  const lastVal = value[value.length - 1];
+  if (lastVal && lastVal.search(/[0-9,]/) !== -1) {
+    value[value.length - 1] += val;
+  } else {
+    value.push(val);
   }
+}
 
-  function push(val) {
-    // if (screen.innerText.length < 12) screen.innerText += val;
-    value += val;
+function clear() {
+  value = [];
+}
+
+function backspace() {
+  // value = value.slice(0, -1);
+}
+
+function add() {
+  const lastVal = value[value.length - 1];
+  if (lastVal && lastVal.search(/[0-9,]/) !== -1) {
+    value.push('+');
   }
+}
 
-  function clear() {
-    value = '';
+function equals() {
+  for (let i = 0; i < value.length; i++) {
+
   }
+  // value = parseFloat(value) + parseFloat(prevValue);
+}
 
-  function backspace() {
-    value = value.slice(0, -1);
-  }
-
-  document.getElementById('container').onclick = function aaa(event) {
-    const tgt = event.target.classList;
-    if (tgt.contains('number')) push(event.target.innerText);
-    else if (tgt.contains('clear')) clear();
-    else if (tgt.contains('backspace')) backspace();
-    formatSetScreen();
-  };
-}());
+document.getElementById('container').onclick = (event) => {
+  const tgt = event.target.classList;
+  if (tgt.contains('number')) push(event.target.innerText);
+  else if (tgt.contains('clear')) clear();
+  else if (tgt.contains('backspace')) backspace();
+  else if (tgt.contains('add')) add();
+  else if (tgt.contains('equals')) equals();
+  formatSetScreen();
+};
